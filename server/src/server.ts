8b48f03fa241cc538ab7e802242c538523082b6f
alanams97 <alan_ams97@gmail.com>
2020-06-03 01:03:07 -0300

    feat: Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-
-
-    res.json([
-        'Alan',
-        'Aline',
-        'Gustavo'
-    ] );
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
